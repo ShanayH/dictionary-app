f230cdbb37d09820705887f98208a0b172ab304d
ShanayH <shanay.taylor@hotmail.com>
2023-01-26 15:11:25 -0600

    Initial Commit
    installed react, bootstrap. Remove unnecessary files.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <div>hello world </div>
+      <button type="button" class="btn btn-primary">
+        Hello again
+      </button>
+    </div>
+  );
+}
+
+export default App;
